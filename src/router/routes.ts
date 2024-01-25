f429e8c52be7226145b4a6a2e2a7a5b4cc68b184
jonathan-nadeau <jonathannadeau.jn@gmail.com>
2024-01-25 15:28:59 -0500

    Remove IndexPage component and update routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,10 +1,22 @@
 import { RouteRecordRaw } from 'vue-router';
 
+const HomePage = () => import('pages/HomePage.vue');
+const AboutPage = () => import('pages/AboutPage.vue');
+const SkillsPage = () => import('pages/SkillsPage.vue');
+const ProjectsPage = () => import('pages/ProjectsPage.vue');
+const ContactPage = () => import('pages/ContactPage.vue');
+
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }]
+    children: [
+      { path: '', name: 'IndexPage', component: HomePage },
+      { path: '/about', name: 'AboutPage', component: AboutPage },
+      { path: '/skills', name: 'SkillsPage', component: SkillsPage },
+      { path: '/projects', name: 'ProjectsPage', component: ProjectsPage },
+      { path: '/contact', name: 'ContactPage', component: ContactPage }
+    ]
   },
 
   // Always leave this as last one,
