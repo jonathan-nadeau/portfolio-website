444be5b8c98b4a48ebd482e4247859a44f34b2d8
jonathan-nadeau <jonathannadeau.jn@gmail.com>
2024-01-25 15:27:30 -0500

    Add new pages to index.ts

---
+++ src/pages/AboutPage.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>About</div>
+</template>
+
+<script lang="ts"></script>
+
+<style lang="sass" scoped></style>
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,7 @@
+export { default as AboutPage } from './AboutPage.vue';
+export { default as ContactPage } from './ContactPage.vue';
 export { default as ErrorNotFound } from './ErrorNotFound.vue';
+export { default as HomePage } from './HomePage.vue';
 export { default as IndexPage } from './IndexPage.vue';
+export { default as ProjectsPage } from './ProjectsPage.vue';
+export { default as SkillsPage } from './SkillsPage.vue';
